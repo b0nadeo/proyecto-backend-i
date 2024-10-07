acd79ff0eb82cdc55e0fe6089711af583ebae3a4
Ezequiel Bonadeo <150759131+b0nadeo@users.noreply.github.com>
2024-10-07 20:05:13 -0300

    Challenge2v2 (#3)
    * segunda preentrega
    
    * arreglo de las imagenes al agregar producto

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -164,7 +164,9 @@ async function createProduct(req, res, next) {
       category: req.body.category,
       price: req.body.price,
       stock: req.body.stock,
-      imageUrl: req.body.imageUrl, 
+
+      img: req.body.img, 
+
     };
 
     await productsManager.create(newProduct); // Crear un nuevo producto
